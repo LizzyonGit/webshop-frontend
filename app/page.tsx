a4f6d1a77f448ebc502d829e24d7e4cd36df9863
Leo Leksell <279720782+leo98lxl@users.noreply.github.com>
2026-09-25 11:32:02 +0200

    added logos

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import NavigationBar from "@/components/navigation-bar";
 
 export default async function Home() {
     return (
-        <main className="min-h-screen bg-gray-50">
+        <main className="min-h-screen">
             <div className="container max-w-7xl mx-auto px-6 py-6">
                 <NavigationBar />
             </div>
--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -7,17 +7,25 @@ import {
     NavigationMenuList,
     navigationMenuTriggerStyle    
 } from "./ui/navigation-menu";
+import Image from "next/image";
 
 export default function NavigationBar() {
     return (
-        <NavigationMenu className="w-full max-w-none border-be border-(var(--border))">
+        <NavigationMenu className="w-full max-w-none border-be-3">
             <NavigationMenuList className="w-full justify-between">
                 <NavigationMenuItem className="flex w-full items-center justify-between">
 
                     <NavigationMenuLink
                     render={<Link href="/" />}
                     className={navigationMenuTriggerStyle()}
-                    >Sky Market {/*+ logo*/}
+                    >{<Image 
+                        src="/sky-market-transparent.png" 
+                        alt="Sky Market logo"
+                        width={200}
+                        height={100}
+                        priority
+                        className="h-auto w-[160px] object-contain"
+                        />}
                     </NavigationMenuLink>
 
                     <div className="flex items-center gap-2">
@@ -36,7 +44,14 @@ export default function NavigationBar() {
                         <NavigationMenuLink
                         render={<Link href="#" />}
                         className={navigationMenuTriggerStyle()}
-                        >Your Cart {/*replace w. logo*/}
+                        >{<Image 
+                            src="/shopping-cart.svg" 
+                            alt="Shopping cart logo"
+                            width={24}
+                            height={24}
+                            priority
+                            className="h-auto w-[24px] object-contain dark:invert"
+                            />}
                         </NavigationMenuLink>
                     </div>
 
